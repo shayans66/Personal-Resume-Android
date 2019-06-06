57cb891cf4d5ef3c5ae38cf1dd6f446ed21bd1c0
shayans66 <42shayan42@gmail.com>
2019-06-06 15:49:37 -0700

    hi

--- app/src/main/java/me/shayansarn/personal_resume/MainActivity.java
+++ app/src/main/java/me/shayansarn/personal_resume/MainActivity.java
@@ -1,13 +1,30 @@
 package me.shayansarn.personal_resume;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button workHistoryButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        workHistoryButton = findViewById(R.id.workHistoryButton);
+
+        workHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToTheOtherActivity = new Intent(getApplicationContext(), WorkHistoryActivity.class);
+                startActivity(goToTheOtherActivity);
+            }
+        });
     }
 }
