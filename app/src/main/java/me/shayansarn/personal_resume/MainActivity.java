e7ecdb913465eb0d8fc04264fbb99ed0c7b52c96
shayans66 <42shayan42@gmail.com>
2019-06-06 16:24:12 -0700

    Finished mostly

--- app/src/main/java/me/shayansarn/personal_resume/MainActivity.java
+++ app/src/main/java/me/shayansarn/personal_resume/MainActivity.java
@@ -1,6 +1,7 @@
 package me.shayansarn.personal_resume;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.ViewAnimator;
 public class MainActivity extends AppCompatActivity {
 
     Button workHistoryButton;
+    Button callButton;
+    Button emailButton;
+
 
 
     @Override
@@ -18,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         workHistoryButton = findViewById(R.id.workHistoryButton);
+        callButton = findViewById(R.id.callButton);
+        emailButton = findViewById(R.id.emailButton);
+
 
         workHistoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +33,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(goToTheOtherActivity);
             }
         });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri myPhoneNumber = Uri.parse("tel: 4087138570");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, myPhoneNumber);
+                startActivity(callIntent);
+            }
+        });
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"42shayan42@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Shayan's Resume");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "I really like your resume!");
+                startActivity(emailIntent);
+
+            }
+        });
+
     }
 }
